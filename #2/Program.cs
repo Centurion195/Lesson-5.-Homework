57c0d879ce6024710f1204be7122b322c1b63de6
Centurion195 <Vadim-0211@yandex.ru>
2023-02-10 20:49:37 +0300

    Task #2 completed

---
+++ #2/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Написать программу масштабирования фигуры. Тут для тех кто далеко улетел, чтобы задавались вершины фигуры списком (одной строкой)
+например: "(0,0) (2,0) (2,2) (0,2)"
+коэффициент масштабирования k задавался отдельно - 2 или 4 или 0.5
+В результате показать координаты, которые получатся.
+при k = 2 получаем "(0,0) (4,0) (4,4) (0,4)"
+*/
+Console.Clear();
+
+int[] readCoordinates(){
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((i%2==0) && (i==0 || i==1)) Console.WriteLine("Точка А");
+        else if ((i%2==0) && (i==2 || i==3)) Console.WriteLine("Точка B");
+        else if ((i%2==0) && (i==4 || i==5)) Console.WriteLine("Точка C");
+        else if ((i%2==0) && (i==6 || i==7)) Console.WriteLine("Точка D");
+
+        Console.WriteLine(i%2==0 ? "Введите координату x:" : "Введите координату y:");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int[] array = readCoordinates();
+Console.WriteLine($"Координаты: ({array[0]},{array[1]}) ({array[2]},{array[3]}) ({array[4]},{array[5]}) ({array[6]},{array[7]})");
+Console.WriteLine("Введите коэффициент масштабирования:");
+int k = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Новые координаты: ({array[0]*k},{array[1]*k}) ({array[2]*k},{array[3]*k}) ({array[4]*k},{array[5]*k}) ({array[6]*k},{array[7]*k})");
